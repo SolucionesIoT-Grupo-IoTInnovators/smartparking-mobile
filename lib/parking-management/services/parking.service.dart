81d6c6509b59b40a62691bcece9558abb54d2eda
JP19-03 <u202014511@upc.edu.pe>
2025-05-26 18:11:35 -0500

    feat(parking): add method to fetch parking spots by parking ID
    Co-Authored-By: Josty Tafur <89022172+JostyTafur@users.noreply.github.com>
    Co-Authored-By: U202114545 <102629180+U202114545@users.noreply.github.com>
    Co-Authored-By: anonimat0 <103579781+anonimat0@users.noreply.github.com>
    Co-Authored-By: Ernesto Zarate <112828936+ern23zc@users.noreply.github.com>

--- lib/parking-management/services/parking.service.dart
+++ lib/parking-management/services/parking.service.dart
@@ -1,7 +1,35 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:smartparking_mobile_application/shared/services/http-common.dart';
 
 class ParkingService extends ApiClient {
   ParkingService() {
     resourceEndPoint = '/parkings';
+  }
+
+  Future<Map<String, String>> _getHeaders() async {
+    final token = await getToken();
+    if (token == null) throw Exception('Token not found');
+
+    return {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $token',
+    };
+  }
+
+  Future<List<dynamic>> getParkingSpotsByParkingId(int parkingId) async {
+    final uri = Uri.parse('$baseUrl$resourceEndPoint/$parkingId/spots');
+    final headers = await _getHeaders();
+    final response = await http.get(uri, headers: headers);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(utf8.decode(response.bodyBytes));
+    } else if (response.statusCode == 404) {
+      print('No data available');
+      return [];
+    } else {
+      throw Exception('Error loading data: ${response.reasonPhrase}');
+    }
   }
 }
